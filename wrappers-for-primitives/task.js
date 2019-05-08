e64cacdba4976facc54e3d05e36cf07f3c45e8ff
Nikolas Drozhzhin <drozhzhinnm@gmail.com>
2019-05-09 01:03:38 +0500

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,8 +1,9 @@
+"use strict";
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
-    let date = window.date.value;
+    let date = new Date(window.date.value);
 
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
@@ -10,8 +11,32 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    
+    function checkData (parameter){
+        for (let key in parameter) {         
+            if (isNaN(parseInt(parameter[key]))) {
+                console.log(`Параметр ${key} содержит неправильное значение ${parameter[key]} `);
+                return `Параметр ${key} содержит неправильное значение ${parameter[key]} `;
+            }
+        } 
+    }
+    
+    checkData ({
+        percent: percent,
+        contribution: contribution,
+        amount: amount
+    })
+    
+    let S = amount - contribution;
+    let P = percent / 100 / 12;
+    let now = new Date();
+    let n = ((date.getMonth() - now.getMonth()) + (12 * (date.getFullYear() - now.getFullYear())));
+    let payMonth = S * (P + P / (((1 + P) ** n) - 1));
+    
+    let totalAmount = payMonth * n;
 
-    // код для задачи №1 писать здесь
+    console.log( totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
     //return totalAmount;
 }
 
@@ -23,6 +48,12 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
+    if (typeof name === "string" && name != "") {
+        console.log(`Привет, мир! Меня зовут ${name}.`);
+        return `Привет, мир! Меня зовут ${name}.`;
+      }else{
+        console.log(`Привет, мир! Меня зовут Аноним.`);
+        return `Привет, мир! Меня зовут Аноним.`;
+      }
     //return greeting;
 }
